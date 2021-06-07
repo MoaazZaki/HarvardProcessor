d8a642d0c033e4c6617cec3fe0fcb16f06b7d4cb
mohammedgrey <mohammeddsaadd@yahoo.com>
2021-06-07 14:44:14 +0200

    ALU set up with NOP,SETC,CLRC, and MOV operation.

--- ALU.vhd
+++ ALU.vhd
@@ -1,73 +1,50 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-entity ALU is 
-port(
-A : in std_logic_vector(15 downto 0);
-B : in std_logic_vector(15 downto 0);
-Cin : in std_logic;
-F : out std_logic_vector(15 downto 0);
-Cout : out std_logic;
-s : in std_logic_vector(3 downto 0));
+ENTITY ALU IS
+    PORT (
+        -- INPUT
+        operand1 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        operand2 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        flagsIN : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+        operation : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        func : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+        -- OUTPUT
+        result : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        flagsOUT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+    );
 
-end entity;
+END ENTITY;
 
 ARCHITECTURE struct OF ALU IS
-
-COMPONENT partA IS
-PORT (
-A : in std_logic_vector(15 downto 0);
-B : in std_logic_vector(15 downto 0);
-Cin : in std_logic;
-F : out std_logic_vector(15 downto 0);
-Cout : out std_logic;
-s : in std_logic_vector(1 downto 0));
-END COMPONENT ;
-
-COMPONENT partB IS
-PORT (
-A : in std_logic_vector(15 downto 0);
-B : in std_logic_vector(15 downto 0);
-F : out std_logic_vector(15 downto 0);
-s : in std_logic_vector(1 downto 0));
-END COMPONENT ;
-
-COMPONENT partC IS
-PORT (
-A : in std_logic_vector(15 downto 0);
-Cin : in std_logic;
-F : out std_logic_vector(15 downto 0);
-Cout : out std_logic;
-s : in std_logic_vector(1 downto 0));
-END COMPONENT ;
-
-COMPONENT partD IS
-PORT (
-A : in std_logic_vector(15 downto 0);
-Cin : in std_logic;
-F : out std_logic_vector(15 downto 0);
-Cout : out std_logic;
-s : in std_logic_vector(1 downto 0));
-END COMPONENT ;
-
-SIGNAL F1,F2,F3,F4 : std_logic_vector(15 downto 0);
-
-SIGNAL Cout1,Cout2,Cout3,Cout4 : std_logic;
+    --declare any temp signals here
 BEGIN
-a1: partA PORT MAP(A,B,Cin,F1,Cout1,s(1 downto 0));
-b1: partB PORT MAP(A,B,F2,s(1 downto 0));
-c1: partC PORT MAP(A,Cin,F3,Cout3,s(1 downto 0));
-d1: partD PORT MAP(A,Cin,F4,Cout4,s(1 downto 0));
-
-F<= F1 when s(3 downto 2) = "00"
-ELSE F2 when s(3 downto 2) = "01"
-ELSE F3 when s(3 downto 2) = "10"
-ELSE F4 when s(3 downto 2) = "11";
-
-Cout<= Cout1 when s(3 downto 2) = "00"
-ELSE Cout2 when s(3 downto 2) = "01"
-ELSE Cout3 when s(3 downto 2) = "10"
-ELSE Cout4 when s(3 downto 2) = "11";
-
-
+    PROCESS (operand1, operand2, operation, func) -- if flagsIN should be included make it (all)
+    BEGIN
+        IF (operation = "00000") THEN --ONE-OPERAND OPERATIONS
+            IF (func = "00000") THEN --No operation
+                flagsOUT <= "000";
+            ELSIF (func = "00001") THEN --Set carry
+                flagsOUT(2) <= '1';
+            ELSIF (func = "00010") THEN --Clear carry
+                flagsOUT(2) <= '0';
+            ELSIF (func = "00011") THEN --Not
+                result <= NOT (operand1);
+            ELSIF (func = "00100") THEN --increment 
+            ELSIF (func = "00101") THEN --decrement
+            ELSIF (func = "00110") THEN --out
+            ELSE --in
+            END IF;
+        ELSE --TWO-OPERAND OPERATIONS
+            IF (operation = "00001" OR operation = "01110") THEN --Move (either register or immediate)
+                result <= operand1;
+            ELSIF (operation = "00011") THEN --Sub
+            ELSIF (operation = "00100") THEN --And
+            ELSIF (operation = "00101") THEN --Or
+            ELSIF (operation = "00110") THEN --Sheft left 
+            ELSIF (operation = "00111") THEN --Sheft right
+            ELSE --Add (this is for ALU operations and even memory operations as well)
+            END IF;
+        END IF;
+    END PROCESS;
 END struct;
